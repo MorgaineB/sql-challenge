174d71eb203c648f736da01fd58c6e42c201aa97
Morgaine B <morgainebowers@gmail.com>
2021-05-01 15:19:16 -0400

    3rd Query Done

--- EmployeeSQL/Employee_Queries.sql
+++ EmployeeSQL/Employee_Queries.sql
@@ -8,3 +8,9 @@ SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date LIKE '%1986';
 
+--List the Manager of each Department with the Following Information: Department Number, Department Name, the Manager's
+--Employee Number, Last Name, and First Name. 
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_manager dm
+JOIN departments AS d ON dm.dept_no = d.dept_no
+JOIN employees AS e ON dm.emp_no = e.emp_no;
