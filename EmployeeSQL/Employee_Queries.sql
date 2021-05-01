682ac3d688b8d9459b79d9032de39b6bfce0f734
Morgaine B <morgainebowers@gmail.com>
2021-05-01 15:08:25 -0400

    New ERD and Table Schema to change DATE variables to VARCHAR

--- EmployeeSQL/Employee_Queries.sql
+++ EmployeeSQL/Employee_Queries.sql
@@ -3,3 +3,8 @@ SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
 JOIN salaries AS s ON s.emp_no = e.emp_no;
 
+--List First Name, Last Name and Hire Date for Employees who were Hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date LIKE '%1986';
+
--- EmployeeSQL/Table_Schema.sql
+++ EmployeeSQL/Table_Schema.sql
@@ -10,11 +10,11 @@ CREATE TABLE employees(
 	emp_no INT PRIMARY KEY,
 	emp_title_id VARCHAR(30),
 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
-	birth_date DATE,
+	birth_date VARCHAR(30),
 	first_name VARCHAR(30),
 	last_name VARCHAR(30),
 	sex VARCHAR(30),
-	hire_date DATE);
+	hire_date VARCHAR(30);
 
 CREATE TABLE dept_emp(
 	emp_no INT,
