0fa504d18f6a9a821425de6541d489955798e2a4
Morgaine B <morgainebowers@gmail.com>
2021-05-01 15:41:37 -0400

    Final Query Done

--- EmployeeSQL/Employee_Queries.sql
+++ EmployeeSQL/Employee_Queries.sql
@@ -42,3 +42,11 @@ FROM employees e
 JOIN dept_emp AS de ON e.emp_no = de.emp_no
 JOIN departments AS d ON de.dept_no = d.dept_no
 WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
+
+--In Descending Order, list the Frequency Count of Employee Last Name
+SELECT last_name, COUNT (*) AS "Frequency Count"
+FROM employees
+GROUP BY last_name
+ORDER BY "Frequency Count";
+
+
